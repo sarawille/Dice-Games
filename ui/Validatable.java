4509bda332b38a56c1605d3b0ea12cebb9a627de
Sara Wille <wille.sk@gmail.com>
2016-04-13 12:56:54 -0400

    Initial commit

---
+++ ui/Validatable.java
@@ -0,0 +1,109 @@
+package ui;
+
+import ui.Displayable;
+import ui.IOFactory;
+
+/**
+ * @author Sara Wille
+ * The Validatable interface confirms whether user input
+ * is a valid int, double or String.  It can also check whether
+ * the int or double is within a certain range, and whether
+ * the String is a given length.
+ *
+ */
+public interface Validatable {
+	Displayable screen = IOFactory.getDisplayable();
+	//TODO pass main displayable into validator and use that to print anything (instead of print in validatable)
+	//dependency inversion pattern
+//	default int isValidInt(String s) {
+//		return Integer.parseInt(s);
+//	}
+	
+	default int isValidInt(String s) {
+		while(true) {
+			try {
+				screen.display(s);
+				int userInteger = Integer.parseInt(s);
+				return userInteger;
+			}
+			catch (NumberFormatException e) {
+				screen.displayln("Error! Invalid integer value. Try again.");
+			}
+		}
+	}
+	
+	default boolean isInRange(int userInt, int min, int max)
+	{
+		if (userInt < min) {
+			screen.displayln("Error! Number must be greater than "
+							+ min);
+			return false;
+		} else if (userInt > max) {
+			screen.displayln("Error! Number must be less than "
+					+ max);
+			return false;
+		} 
+		return true;
+	}
+	
+	default double isValidDouble(String s) {
+		while(true)
+		{
+			try {
+				screen.display(s);
+				double userDouble = Double.parseDouble(s);
+				return userDouble;
+			}
+			catch (NumberFormatException e) {
+				screen.display("Error! Invalid decimal value. Try again.");
+			}
+		}
+	}
+	
+	default boolean isInRange(double userDouble, double min, double max)
+	{
+		if (userDouble < min) {
+			screen.displayln("Error! Number must be greater than "
+							+ min);
+			return false;
+		} else if (userDouble > max) {
+			screen.displayln("Error! Number must be less than "
+					+ max);
+			return false;
+		} 
+		return true;
+	}
+	
+	default String isValidString(String s) {
+		while (true) {
+			if (s.length() > 0) {
+				return s;
+			} else {
+				screen.displayln("Error! This entry is required. Try again.");
+			}
+		}
+	}
+
+	default String isValidString(String s, int len) {
+		while (true) {
+			if (s.length() == len) {
+				return s;
+			} else {
+				screen.displayln("Error! This entry is required to be length "
+								+ len + ". Try again.");
+			}
+		}
+	}
+
+	default String isValidString(String s, String option1, String option2) {
+		while (true) {
+			if (s.equalsIgnoreCase(option1) || s.equalsIgnoreCase(option2)) {
+				return s;
+			} else {
+				screen.displayln("Error! Please enter " + option1 + " or " + option2 + ". Try again.");
+			}
+		}
+	}
+
+
+}
---
+++ ui/Validator.java
@@ -0,0 +1,144 @@
+package ui;
+
+import ui.RequestInformation;
+
+
+/**
+ * @author Sara Wille
+ * The Validator class can get an integer, double or String from the user.
+ * It implements the Validatable class to confirm that the user input
+ * is valid given the type of data requested. 
+ */
+
+public class Validator implements Validatable {
+	static String userInput = "";
+	static int userInteger = 0;
+	static double userDouble = 0;
+	static String userString = "";
+	Displayable screen;
+	
+	public Validator(Displayable screen) {
+		this.screen = screen;
+	}
+	
+	/**
+	 * getInt - ask the user for an integer and confirm that it is a valid integer
+	 * @param s
+	 * @return userInteger
+	 */
+//	public int getInt(String prompt) {
+//		while (true) {
+//			try {
+//				screen.display(prompt);
+//				userInput = RequestInformation.getInfo();
+//				userInteger = isValidInt(userInput);
+//			}
+//			catch (NumberFormatException e) {
+//				screen.displayln("Error! Invalid integer value. Try again.");
+//			}
+//			
+//			
+//			return userInteger;
+//		}
+//	}
+	public int getInt(String prompt) {
+		while (true) {
+			userInput = RequestInformation.getInfo();
+			userInteger = isValidInt(userInput);
+			return userInteger;
+		}
+	}
+	
+
+	/**
+	 * getInt - ask the user for an integer, confirm that it is a valid integer, and 
+	 * confirm that it is within the range min-max
+	 * @param prompt
+	 * @param min
+	 * @param max
+	 * @return userInteger
+	 */
+	public int getInt(String prompt, int min, int max) {
+		while (true) {
+			userInput = RequestInformation.getInfo();
+			userInteger = isValidInt(userInput);
+			if (isInRange(userInteger, min, max)) {
+				return userInteger;
+			}
+		}
+	}
+	
+	/**
+	 * getDouble - ask the user for a double and confirm that it is a valid double
+	 * @param s
+	 * @return userDouble
+	 */
+	public double getDouble(String prompt) {
+		while (true) {
+			userInput = RequestInformation.getInfo();
+			userDouble = isValidDouble(userInput);
+			return userDouble;
+		}
+	}
+	
+	/**
+	 * getDouble - ask the user for a double, confirm that it is a valid double, and 
+	 * confirm that it is within the range min-max
+	 * @param prompt
+	 * @param min
+	 * @param max
+	 * @return userDouble
+	 */
+	public double getDouble(String prompt, double min, double max) {
+		while (true) {
+			userInput = RequestInformation.getInfo();
+			userDouble = isValidDouble(userInput);
+			if (isInRange(userDouble, min, max)) {
+				return userDouble;
+			}
+		}
+	}
+
+	
+	/**
+	 * getString - ask the user for a String, confirm that they entered something
+	 * @param prompt
+	 * @return userString
+	 */
+	public String getString(String prompt) {
+		while (true) {
+			userInput = RequestInformation.getInfo();
+			userString = isValidString(userInput);
+			return userString;
+		}
+	}
+	
+	/**
+	 * getString - ask the user for a String, confirm that they entered a String of the right length
+	 * @param prompt
+	 * @param len
+	 * @return userString
+	 */
+	public String getString(String prompt, int len) {
+		while (true) {
+			userInput = RequestInformation.getInfo();
+			userString = isValidString(userInput, len);
+			return userString;
+		}
+	}
+	
+	/**
+	 * getString - ask the user for a String, confirm that they entered a one of the String options
+	 * @param prompt
+	 * @param option1
+	 * @param option2
+	 * @return userString
+	 */
+	public String getString(String prompt, String option1, String option2) {
+		while (true) {
+			userInput = RequestInformation.getInfo();
+			userString = isValidString(userInput, option1, option2);
+			return userString;
+		}
+	}
+}
