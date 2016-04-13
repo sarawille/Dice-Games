00903fb91d731b0f0f0b6ae773226d5d6b8f9ebe
Sara Wille <wille.sk@gmail.com>
2016-04-13 19:36:41 -0400

    take out numberOfRolls local variable

--- ui/YatzeeApp.java
+++ ui/YatzeeApp.java
@@ -20,12 +20,11 @@ public class YatzeeApp {
 		Hand thisHand; 
 		
 		while (turn <= 5) {
-			int numberOfRolls = 1;
 			screen.displayln("TURN " + turn);
 			thisHand = new Hand();
 			thisHand.rollAll();
 			screen.displayln(thisHand.printHand());
-			numberOfRolls = rerollHand(numberOfRolls, thisHand);
+			rerollHand(thisHand);
 			myScore.scoreHand(thisHand);
 			myScore.printTotalScore();
 			turn++;
@@ -34,21 +33,18 @@ public class YatzeeApp {
 		
 	}
 
-	private static int rerollHand(int numberOfRolls, Hand myHand) {
+	private static void rerollHand(Hand myHand) {
 		String userInput;
-		while (numberOfRolls < 3) {
+		while (myHand.numberOfRolls < 3) {
 			userInput = theValidator.getString("\nWould you like to roll again? (y/n) ", "y", "n");
-
 			if (userInput.equalsIgnoreCase("y")) {
 				rollAgain(myHand);
 				screen.displayln(myHand.printHand());
-				numberOfRolls++;
 			} else {
-				myHand.printHand();
+				screen.displayln(myHand.printHand());
 				break;
 			}
 		}
-		return numberOfRolls;
 	}
 	
 	private static void rollAgain(Hand newHand) 
