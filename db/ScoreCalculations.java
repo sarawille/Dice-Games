71f08227588be046988e49e2c3be68afc06bd570
Sara Wille <wille.sk@gmail.com>
2016-04-13 19:23:48 -0400

    Add scoreHand()

--- db/ScoreCalculations.java
+++ db/ScoreCalculations.java
@@ -18,19 +18,19 @@ public class ScoreCalculations extends ScoreEquations implements Scorable {
 		this.screen = myDisplay;
 	}
 	
-//	public void scoreHand(Hand newHand) {
-//		ScoreCategory scoreCategoryForThisHand = null;
-//
-//		//determine which categories are eligible for scoring based on the hand
-//		execute(newHand);
-//		
-//		//print the eligible score categories and ask the user to pick one
-//		screen.displayln("\n" + scoreChoices);
-//		scoreCategoryForThisHand = assignScoreCategory();
-//		
-//		//incorporate the score for this hand into the total score
-//		calcTotalScore(scoreCategoryForThisHand);
-//	}
+	public void scoreHand(Hand newHand) {
+		ScoreCategory scoreCategoryForThisHand = null;
+
+		//determine which categories are eligible for scoring based on the hand
+		execute(newHand);
+		
+		//print the eligible score categories and ask the user to pick one
+		screen.displayln("\n" + scoreChoices);
+		scoreCategoryForThisHand = assignScoreCategory();
+		
+		//incorporate the score for this hand into the total score
+		calcTotalScore(scoreCategoryForThisHand);
+	}
 	
 	public ScoreCategory assignScoreCategory() {
 		String userInput = "";
@@ -73,75 +73,75 @@ public class ScoreCalculations extends ScoreEquations implements Scorable {
 				case "y":
 					return ScoreCategory.YAHTZEE;
 				default:
-					screen.display("\nPlease choose a valid scoring option.");
+					screen.display("Please choose a valid scoring option.\n");
 			}
 		}
 	}
 
 	@Override
 	public void calcTotalScore(ScoreCategory userChoice) {
-//		switch(userChoice)
-//		{
-//		case ONES:
-//			totalScore += onesPossiblePoints;
-//			onesPoints = onesPossiblePoints;
-//			break;
-//		case TWOS:
-//			totalScore += twosPossiblePoints;
-//			twosPoints = twosPossiblePoints;
-//			break;
-//		case THREES:
-//			totalScore += threesPossiblePoints;
-//			threesPoints = threesPossiblePoints;
-//			break;
-//		case FOURS:
-//			totalScore += foursPossiblePoints;
-//			foursPoints = foursPossiblePoints;
-//			break;
-//		case FIVES:
-//			totalScore += fivesPossiblePoints;
-//			fivesPoints = fivesPossiblePoints;
-//			break;
-//		case SIXES:
-//			totalScore += sixesPossiblePoints;
-//			sixesPoints = sixesPossiblePoints;
-//			break;
-//		case THREE_OF_A_KIND:
-//			totalScore += threeOfAKindPossiblePoints;
-//			threeOfAKindPoints = threeOfAKindPossiblePoints;
-//			break;
-//		case FOUR_OF_A_KIND:
-//			totalScore += fourOfAKindPossiblePoints;
-//			fourOfAKindPoints = fourOfAKindPossiblePoints;
-//			break;
-//		case FULL_HOUSE:
-//			totalScore += fullHousePossiblePoints;
-//			fullHousePoints = fullHousePossiblePoints;
-//			break;
-//		case SMALL_STRAIGHT:
-//			totalScore += smallStraightPossiblePoints;
-//			smallStraightPoints = smallStraightPossiblePoints;
-//			break;
-//		case LARGE_STRAIGHT:
-//			totalScore += largeStraightPossiblePoints;
-//			largeStraightPoints = largeStraightPossiblePoints;
-//			break;
-//		case CHANCE:
-//			totalScore += chancePossiblePoints;
-//			chancePoints = chancePossiblePoints;
-//			break;
-//		case YAHTZEE:
-//			totalScore += yahtzeePossiblePoints;
-//			yahtzeePoints += yahtzeePossiblePoints;
-//			break;
-//		default:
-//			break;
-//		}	
+		switch(userChoice)
+		{
+		case ONES:
+			totalScore += onesPossiblePoints;
+			onesPoints = onesPossiblePoints;
+			break;
+		case TWOS:
+			totalScore += twosPossiblePoints;
+			twosPoints = twosPossiblePoints;
+			break;
+		case THREES:
+			totalScore += threesPossiblePoints;
+			threesPoints = threesPossiblePoints;
+			break;
+		case FOURS:
+			totalScore += foursPossiblePoints;
+			foursPoints = foursPossiblePoints;
+			break;
+		case FIVES:
+			totalScore += fivesPossiblePoints;
+			fivesPoints = fivesPossiblePoints;
+			break;
+		case SIXES:
+			totalScore += sixesPossiblePoints;
+			sixesPoints = sixesPossiblePoints;
+			break;
+		case THREE_OF_A_KIND:
+			totalScore += threeOfAKindPossiblePoints;
+			threeOfAKindPoints = threeOfAKindPossiblePoints;
+			break;
+		case FOUR_OF_A_KIND:
+			totalScore += fourOfAKindPossiblePoints;
+			fourOfAKindPoints = fourOfAKindPossiblePoints;
+			break;
+		case FULL_HOUSE:
+			totalScore += fullHousePossiblePoints;
+			fullHousePoints = fullHousePossiblePoints;
+			break;
+		case SMALL_STRAIGHT:
+			totalScore += smallStraightPossiblePoints;
+			smallStraightPoints = smallStraightPossiblePoints;
+			break;
+		case LARGE_STRAIGHT:
+			totalScore += largeStraightPossiblePoints;
+			largeStraightPoints = largeStraightPossiblePoints;
+			break;
+		case CHANCE:
+			totalScore += chancePossiblePoints;
+			chancePoints = chancePossiblePoints;
+			break;
+		case YAHTZEE:
+			totalScore += yahtzeePossiblePoints;
+			yahtzeePoints += yahtzeePossiblePoints;
+			break;
+		default:
+			break;
+		}	
 	}
 
 	@Override
 	public void printTotalScore() {
-//		screen.displayln("Your total score is " + totalScore);
+		screen.displayln("Your total score is " + totalScore);
 	}
 
 }
