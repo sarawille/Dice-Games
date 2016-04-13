6325e6bac51f1513c3cfb0d2018ed924b011bce7
Sara Wille <wille.sk@gmail.com>
2016-04-13 11:56:05 -0400

    Add ScoreCalculaions and TestScoreCalculatons

---
+++ db/ScoreCalculations.java
@@ -0,0 +1,20 @@
+package db;
+
+import db.Scorable;
+import db.ScoreEquations;
+
+public class ScoreCalculations extends ScoreEquations implements Scorable {
+
+	@Override
+	public void calcTotalScore(ScoreCategory userChoice) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void printTotalScore() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
