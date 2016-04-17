a0eb99f0fe26545df9c27d319086ba7eb2ec4a8f
Sara Wille <wille.sk@gmail.com>
2016-04-17 16:08:54 -0400

    countHand and testCountHand methods

---
+++ scoring/ScoreEquations2.java
@@ -0,0 +1,31 @@
+package scoring;
+
+import java.util.HashMap;
+
+import business.Hand;
+
+public class ScoreEquations2 {
+	
+	protected Hand theHand;
+	public HashMap<String, Integer> handValues = new HashMap<>();
+	protected HashMap<String, Integer> scoreCategoryOptions = new HashMap<>();
+	protected StringBuilder scoreChoices = new StringBuilder();
+	
+	public ScoreEquations2(Hand newHand) {
+		theHand = newHand;
+	}
+	
+	public void countHand(Hand newHand) {
+		int instances = 0;
+		for (int faceUp = 1; faceUp <= newHand.diceInHand.size(); faceUp++) {
+			instances = 0;
+			for (int dieNumber = 0; dieNumber < newHand.diceInHand.size(); dieNumber++)
+			{
+				if (newHand.readDie(dieNumber) == faceUp){
+					instances++;
+				}
+			}
+			handValues.put("" + faceUp, instances);
+		}
+	}
+}
--- tests/TestScore2.java
+++ tests/TestScore2.java
@@ -2,20 +2,26 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import business.Hand;
 import scoring.ScoreEquations2;
 
 public class TestScore2 {
 	static ScoreEquations2 newTest;
+	static Hand testHand;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		newTest = new ScoreEquations2();
+		testHand = new Hand(5, 6);
+		newTest = new ScoreEquations2(testHand);
+		
 	}
 
 	@AfterClass
@@ -31,8 +37,36 @@ public class TestScore2 {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCountHand() {
+		HashMap<String, Integer> testHandValues = new HashMap<>();
+		testHandValues.put("1", 1);
+		testHandValues.put("2", 1);
+		testHandValues.put("3", 1);
+		testHandValues.put("4", 0);
+		testHandValues.put("5", 2);
+		
+		while (testHand.diceInHand.get(0).readFaceUp() != 1){
+			testHand.rollDie(0);
+		}
+		while (testHand.diceInHand.get(1).readFaceUp() != 2){
+			testHand.rollDie(1);
+		}
+		while (testHand.diceInHand.get(2).readFaceUp() != 3){
+			testHand.rollDie(2);
+		}
+		while (testHand.diceInHand.get(3).readFaceUp() != 5){
+			testHand.rollDie(3);
+		}
+		while (testHand.diceInHand.get(4).readFaceUp() != 5){
+			testHand.rollDie(4);
+		}
+		newTest.countHand(testHand);
+		
+		assertEquals(testHandValues.get("1"), newTest.handValues.get("1"));
+		assertEquals(testHandValues.get("2"), newTest.handValues.get("2"));
+		assertEquals(testHandValues.get("3"), newTest.handValues.get("3"));
+		assertEquals(testHandValues.get("4"), newTest.handValues.get("4"));
+		assertEquals(testHandValues.get("5"), newTest.handValues.get("5"));
 	}
 
 }
