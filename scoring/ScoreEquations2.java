74e57e956aec0dd48a26e638a1628593247a600a
Sara Wille <wille.sk@gmail.com>
2016-04-17 16:28:36 -0400

    add methods to constructor to fill the scoreBoard and scoreCategoryOptions hash maps

--- scoring/ScoreEquations2.java
+++ scoring/ScoreEquations2.java
@@ -14,6 +14,40 @@ public class ScoreEquations2 {
 	
 	public ScoreEquations2(Hand newHand) {
 		theHand = newHand;
+		setScoreCategoryOptions();
+		setScoreBoard();
+	}
+
+	private void setScoreCategoryOptions() {
+		scoreCategoryOptions.put(ScoreCategory.ONES, 0);
+		scoreCategoryOptions.put(ScoreCategory.TWOS, 0);
+		scoreCategoryOptions.put(ScoreCategory.THREES, 0);
+		scoreCategoryOptions.put(ScoreCategory.FOURS, 0);
+		scoreCategoryOptions.put(ScoreCategory.FIVES, 0);
+		scoreCategoryOptions.put(ScoreCategory.SIXES, 0);
+		scoreCategoryOptions.put(ScoreCategory.THREE_OF_A_KIND, 0);
+		scoreCategoryOptions.put(ScoreCategory.FOUR_OF_A_KIND, 0);
+		scoreCategoryOptions.put(ScoreCategory.FULL_HOUSE, 0);
+		scoreCategoryOptions.put(ScoreCategory.SMALL_STRAIGHT, 0);
+		scoreCategoryOptions.put(ScoreCategory.LARGE_STRAIGHT, 0);
+		scoreCategoryOptions.put(ScoreCategory.CHANCE, 0);
+		scoreCategoryOptions.put(ScoreCategory.YAHTZEE, 0);
+	}
+	
+	private void setScoreBoard() {
+		scoreBoard.put(ScoreCategory.ONES, -1);
+		scoreBoard.put(ScoreCategory.TWOS, -1);
+		scoreBoard.put(ScoreCategory.THREES, -1);
+		scoreBoard.put(ScoreCategory.FOURS, -1);
+		scoreBoard.put(ScoreCategory.FIVES, -1);
+		scoreBoard.put(ScoreCategory.SIXES, -1);
+		scoreBoard.put(ScoreCategory.THREE_OF_A_KIND, -1);
+		scoreBoard.put(ScoreCategory.FOUR_OF_A_KIND, -1);
+		scoreBoard.put(ScoreCategory.FULL_HOUSE, -1);
+		scoreBoard.put(ScoreCategory.SMALL_STRAIGHT, -1);
+		scoreBoard.put(ScoreCategory.LARGE_STRAIGHT, -1);
+		scoreBoard.put(ScoreCategory.CHANCE, -1);
+		scoreBoard.put(ScoreCategory.YAHTZEE, -1);
 	}
 	
 	public void countHandValues(Hand newHand) {
@@ -28,5 +62,10 @@ public class ScoreEquations2 {
 			}
 			handValues.put("" + faceUp, instances);
 		}
+	}
+
+	public void calcUpperScores(Hand testHand) {
+		
+		
 	}
 }
