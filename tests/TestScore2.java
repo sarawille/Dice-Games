e6dba72800966937caf7d7223dc831acfd30a2fd
Sara Wille <wille.sk@gmail.com>
2016-04-17 15:50:52 -0400

    Create new test for alt scoring approach

---
+++ tests/TestScore2.java
@@ -0,0 +1,38 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import scoring.ScoreEquations2;
+
+public class TestScore2 {
+	static ScoreEquations2 newTest;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		newTest = new ScoreEquations2();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
