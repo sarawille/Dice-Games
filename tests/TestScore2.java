b13e42a43a67d2566e8e9c39e8776438d5a4dd31
Sara Wille <wille.sk@gmail.com>
2016-04-17 20:21:36 -0400

    add checks to all test to make sure categories can only be scored once

--- tests/TestScore2.java
+++ tests/TestScore2.java
@@ -79,6 +79,7 @@ public class TestScore2 {
 	
 	@Test
 	public void testCalcUpperScores() {
+		//Test that Upper Scores (Ones, Twos, Three, Fours, Fives, Sixes) will be scored 
 		testScoreCategoryOptions.put(ScoreCategory.ONES, 1*1);
 		testScoreCategoryOptions.put(ScoreCategory.TWOS, 1*2);
 		testScoreCategoryOptions.put(ScoreCategory.THREES, 1*3);
@@ -117,10 +118,48 @@ public class TestScore2 {
 					 newTest.scoreCategoryOptions.get(ScoreCategory.FIVES));
 		assertEquals(testScoreCategoryOptions.get(ScoreCategory.SIXES), 
 			 	 	 newTest.scoreCategoryOptions.get(ScoreCategory.SIXES));
+		
+		//Test that Upper Scores cannot be scored more than once
+		newTest.scoreCategoryOptions.put(ScoreCategory.ONES, 0);  //reset possible points
+		newTest.scoreBoard.put(ScoreCategory.ONES, 10);  //player already scored Ones
+		testScoreCategoryOptions.put(ScoreCategory.ONES,0);  //expect 0
+		assertEquals(testScoreCategoryOptions.get(ScoreCategory.ONES), 
+				 newTest.scoreCategoryOptions.get(ScoreCategory.ONES));
+		
+		newTest.scoreCategoryOptions.put(ScoreCategory.TWOS, 0);  //reset possible points
+		newTest.scoreBoard.put(ScoreCategory.TWOS, 10);  //player already scored Twos
+		testScoreCategoryOptions.put(ScoreCategory.TWOS,0);  //expect 0
+		assertEquals(testScoreCategoryOptions.get(ScoreCategory.TWOS), 
+				 newTest.scoreCategoryOptions.get(ScoreCategory.TWOS));
+		
+		newTest.scoreCategoryOptions.put(ScoreCategory.THREES, 0);  //reset possible points
+		newTest.scoreBoard.put(ScoreCategory.THREES, 10);  //player already scored Threes
+		testScoreCategoryOptions.put(ScoreCategory.THREES ,0);  //expect 0
+		assertEquals(testScoreCategoryOptions.get(ScoreCategory.THREES), 
+				 newTest.scoreCategoryOptions.get(ScoreCategory.THREES));
+		
+		newTest.scoreCategoryOptions.put(ScoreCategory.FOURS, 0);  //reset possible points
+		newTest.scoreBoard.put(ScoreCategory.FOURS, 10);  //player already scored Fours
+		testScoreCategoryOptions.put(ScoreCategory.FOURS ,0);  //expect 0
+		assertEquals(testScoreCategoryOptions.get(ScoreCategory.FOURS), 
+				 newTest.scoreCategoryOptions.get(ScoreCategory.FOURS));
+		
+		newTest.scoreCategoryOptions.put(ScoreCategory.FIVES, 0);  //reset possible points
+		newTest.scoreBoard.put(ScoreCategory.FIVES, 10);  //player already scored Fives
+		testScoreCategoryOptions.put(ScoreCategory.FIVES ,0);  //expect 0
+		assertEquals(testScoreCategoryOptions.get(ScoreCategory.FIVES), 
+				 newTest.scoreCategoryOptions.get(ScoreCategory.FIVES));
+		
+		newTest.scoreCategoryOptions.put(ScoreCategory.SIXES, 0);  //reset possible points
+		newTest.scoreBoard.put(ScoreCategory.SIXES, 10);  //player already scored Sixes
+		testScoreCategoryOptions.put(ScoreCategory.SIXES ,0);  //expect 0
+		assertEquals(testScoreCategoryOptions.get(ScoreCategory.SIXES), 
+				 newTest.scoreCategoryOptions.get(ScoreCategory.SIXES));
 	}
 	
 	@Test
 	public void testCalcThreeOfAKind() {
+		//Test that Three of a Kind will be scored 
 		testScoreCategoryOptions.put(ScoreCategory.THREE_OF_A_KIND, 12);
 		
 		while (testHand.diceInHand.get(0).readFaceUp() != 4){
@@ -144,10 +183,19 @@ public class TestScore2 {
 		
 		assertEquals(testScoreCategoryOptions.get(ScoreCategory.THREE_OF_A_KIND), 
 		 	 	 newTest.scoreCategoryOptions.get(ScoreCategory.THREE_OF_A_KIND));
+		
+		//Test that Three of a Kind cannot be scored more than once
+		newTest.scoreCategoryOptions.put(ScoreCategory.THREE_OF_A_KIND, 0);  //reset possible points
+		newTest.scoreBoard.put(ScoreCategory.THREE_OF_A_KIND, 10);  //player already scored 3K
+		testScoreCategoryOptions.put(ScoreCategory.THREE_OF_A_KIND ,0);  //expect 0
+		
+		assertEquals(testScoreCategoryOptions.get(ScoreCategory.THREE_OF_A_KIND), 
+		 	 	 newTest.scoreCategoryOptions.get(ScoreCategory.THREE_OF_A_KIND));
 	}
 	
 	@Test
 	public void testCalcFourOfAKind() {
+		//Test that Four of a Kind will be scored 
 		testScoreCategoryOptions.put(ScoreCategory.FOUR_OF_A_KIND, 20);
 		
 		while (testHand.diceInHand.get(0).readFaceUp() != 5){
@@ -171,10 +219,19 @@ public class TestScore2 {
 		
 		assertEquals(testScoreCategoryOptions.get(ScoreCategory.FOUR_OF_A_KIND), 
 		 	 	 	 newTest.scoreCategoryOptions.get(ScoreCategory.FOUR_OF_A_KIND));
+		
+		//Test that Four of a Kind cannot be scored more than once
+		newTest.scoreCategoryOptions.put(ScoreCategory.FOUR_OF_A_KIND, 0);  //reset possible points
+		newTest.scoreBoard.put(ScoreCategory.FOUR_OF_A_KIND, 10);  //player already scored 4K
+		testScoreCategoryOptions.put(ScoreCategory.FOUR_OF_A_KIND ,0);  //expect 0
+		
+		assertEquals(testScoreCategoryOptions.get(ScoreCategory.FOUR_OF_A_KIND), 
+	 	 	 	 newTest.scoreCategoryOptions.get(ScoreCategory.FOUR_OF_A_KIND));
 	}
 
 	@Test
 	public void testCalcFullHouse() {
+		//Test that Full House will be scored 
 		testScoreCategoryOptions.put(ScoreCategory.FULL_HOUSE, 25);
 		
 		while (testHand.diceInHand.get(0).readFaceUp() != 1){
@@ -198,11 +255,20 @@ public class TestScore2 {
 		
 		assertEquals(testScoreCategoryOptions.get(ScoreCategory.FULL_HOUSE), 
 		 	 	 newTest.scoreCategoryOptions.get(ScoreCategory.FULL_HOUSE));
+		
+		//Test that Small Straight cannot be scored more than once
+		newTest.scoreCategoryOptions.put(ScoreCategory.FULL_HOUSE, 0);  //reset possible points
+		newTest.scoreBoard.put(ScoreCategory.FULL_HOUSE, 25);  //player already scored FH
+		testScoreCategoryOptions.put(ScoreCategory.FULL_HOUSE ,0);  //expect 0
+			
+		assertEquals(testScoreCategoryOptions.get(ScoreCategory.FULL_HOUSE), 
+		 	 	 newTest.scoreCategoryOptions.get(ScoreCategory.FULL_HOUSE));
 	}
 	
 
 	@Test
 	public void testSmallStraight() {
+		//Test that Small Straight will be scored 
 		testScoreCategoryOptions.put(ScoreCategory.SMALL_STRAIGHT, 30);
 		
 		while (testHand.diceInHand.get(0).readFaceUp() != 1){
@@ -226,10 +292,19 @@ public class TestScore2 {
 		
 		assertEquals(testScoreCategoryOptions.get(ScoreCategory.SMALL_STRAIGHT), 
 		 	 	 newTest.scoreCategoryOptions.get(ScoreCategory.SMALL_STRAIGHT));
-	}
+		
+		//Test that Small Straight cannot be scored more than once
+		newTest.scoreCategoryOptions.put(ScoreCategory.SMALL_STRAIGHT, 0);  //reset possible points
+		newTest.scoreBoard.put(ScoreCategory.SMALL_STRAIGHT, 30);  //player already scored SS
+		testScoreCategoryOptions.put(ScoreCategory.SMALL_STRAIGHT ,0);  //expect 0
+		
+		assertEquals(testScoreCategoryOptions.get(ScoreCategory.SMALL_STRAIGHT), 
+		 	 	 newTest.scoreCategoryOptions.get(ScoreCategory.SMALL_STRAIGHT));
+}
 	
 	@Test
 	public void testLargeStraight() {
+		//Test that Large Straight will be scored 
 		testScoreCategoryOptions.put(ScoreCategory.LARGE_STRAIGHT, 40);
 		
 		while (testHand.diceInHand.get(0).readFaceUp() != 1){
@@ -250,6 +325,14 @@ public class TestScore2 {
 		newTest.resetScoreBoard();
 		newTest.countHandValues(testHand);
 		newTest.calcLargeStraight();
+		
+		assertEquals(testScoreCategoryOptions.get(ScoreCategory.LARGE_STRAIGHT), 
+		 	 	 newTest.scoreCategoryOptions.get(ScoreCategory.LARGE_STRAIGHT));
+		
+		//Test that Large Straight cannot be scored more than once
+		newTest.scoreCategoryOptions.put(ScoreCategory.LARGE_STRAIGHT, 0);  //reset possible points
+		newTest.scoreBoard.put(ScoreCategory.LARGE_STRAIGHT, 40);  //player already scored LS
+		testScoreCategoryOptions.put(ScoreCategory.LARGE_STRAIGHT ,0);  //expect 0
 		
 		assertEquals(testScoreCategoryOptions.get(ScoreCategory.LARGE_STRAIGHT), 
 		 	 	 newTest.scoreCategoryOptions.get(ScoreCategory.LARGE_STRAIGHT));
