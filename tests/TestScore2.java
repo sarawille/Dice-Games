fb46388fb6e0d3134306620797691ae8ef20d07f
Sara Wille <wille.sk@gmail.com>
2016-04-19 12:44:30 -0400

    add testCalcLargeStraightOnlyOnce - by breaking off from testCalcLargeStraight

--- tests/TestScore2.java
+++ tests/TestScore2.java
@@ -387,8 +387,27 @@ public class TestScore2 {
 		
 		assertEquals(testScoreCategoryOptions.get(ScoreCategory.LARGE_STRAIGHT), 
 		 	 	 newTest.scoreCategoryOptions.get(ScoreCategory.LARGE_STRAIGHT));
-		
+
+	}
+	
+	@Test
+	public void testCalcLargeStraightOnlyOnce() {
 		//Test that Large Straight cannot be scored more than once
+		while (testHand.diceInHand.get(0).readFaceUp() != 1){
+			testHand.rollDie(0);
+		}
+		while (testHand.diceInHand.get(1).readFaceUp() != 2){
+			testHand.rollDie(1);
+		}
+		while (testHand.diceInHand.get(2).readFaceUp() != 3){
+			testHand.rollDie(2);
+		}
+		while (testHand.diceInHand.get(3).readFaceUp() != 4){
+			testHand.rollDie(3);
+		}
+		while (testHand.diceInHand.get(4).readFaceUp() != 5){
+			testHand.rollDie(4);
+		}
 		newTest.scoreCategoryOptions.put(ScoreCategory.LARGE_STRAIGHT, 0);  //reset possible points
 		newTest.scoreBoard.put(ScoreCategory.LARGE_STRAIGHT, 40);  //player already scored LS
 		testScoreCategoryOptions.put(ScoreCategory.LARGE_STRAIGHT ,0);  //expect 0
