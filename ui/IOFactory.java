eb3e4e9494089bad4641ede658a4e7b120bc3685
Sara Wille <wille.sk@gmail.com>
2016-04-20 08:17:36 -0400

    Singleton pattern for Validator

--- ui/IOFactory.java
+++ ui/IOFactory.java
@@ -5,5 +5,9 @@ public class IOFactory{
 	public static Displayable getDisplayable(){
 		return SimpleDisplay.getInstance();
 	}
+	
+	public static Validator getValidator() {
+		return Validator.getInstance();
+	}
 
 }
